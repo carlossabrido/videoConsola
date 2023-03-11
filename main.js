94831763cae51a226e53f3ffa4abe97f01116c12
Carlos <carlossabrido12@gmail.com>
2023-03-11 19:26:02 +0100

    implementación de j S

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("hola")
+
+function inicio1() {
+    document.getElementById("wow").style.display="flex";
+    document.getElementById("bombi").style.backgroundColor="red";
+
+}
+
+function inicio2() {
+    document.getElementById("wow").style.display="none";
+    document.getElementById("bombi").style.backgroundColor="white";
+
+}
